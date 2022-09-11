590143a6649de61ea29c242166a4355ac84c9356
xuanvietchu <38886630+xuanvietchu@users.noreply.github.com>
2022-09-10 22:05:47 -0600

    PUSH

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+let userCount = 0;
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    userCount++;
+    let num = userCount;
+    socket.on('chat message', (msg) => {
+      io.emit('chat message', `User${num}: ` + msg);
+      });
+
+    socket.on('disconnect', () => {
+      console.log('user disconnected');
+      });
+
+});
+
+server.listen(3000, () => {
+    console.log('listening on *:3000');
+});
